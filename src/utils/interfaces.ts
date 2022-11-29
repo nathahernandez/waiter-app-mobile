18294a39be3a38ab9bcc095c698b21bf987f2610
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-29 12:14:34 -0300

    feat: migrated interfaces

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,21 @@
+export interface TypographyProps {
+	FAMILY: {
+		_400: string;
+		_500: string;
+		_600: string;
+		},
+	SIZE: {
+		XL: string;
+		LG: string;
+		MD: string;
+		SM: string;
+	}
+}
+export interface ColorsProps {
+	RED: string;
+	BLACK: string;
+	WHITE: string;
+}
+export interface BoxShadowProps {
+	ICON: string;
+}
